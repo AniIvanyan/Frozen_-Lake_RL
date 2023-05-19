1a3efff8e1ba29f2ab8b5aab152fbc13228a0ddf
AniIvanyan <aniivanyan23@gmail.com>
2023-05-20 03:20:06 +0400

    Testing MC

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt  
 
+# Learns a value function for an environment using first-visit Monte Carlo control.
 def first_visit_MC(env, stateNumber, numberOfEpisodes, discountRate):
     V = np.zeros(stateNumber)
     returnState = np.zeros(stateNumber)
@@ -9,14 +12,14 @@ def first_visit_MC(env, stateNumber, numberOfEpisodes, discountRate):
     for episode in range(numberOfEpisodes):
         visitedStates = []
         rewardState = []
-        currentState = env.reset()
+        (currentState,_) = env.reset()
         visitedStates.append(currentState)
 
         print("Simulating episode {}".format(episode))
 
         while True:
             randomAction = env.action_space.sample()
-            currentState, currentReward, terminalState = env.step(randomAction)
+            (currentState, currentReward, terminalState,_,_)= env.step(randomAction)
             rewardState.append(currentReward)
             if not terminalState:
                 visitedStates.append(currentState)
@@ -38,3 +41,31 @@ def first_visit_MC(env, stateNumber, numberOfEpisodes, discountRate):
         if visitedStatesNum[i] != 0:
             V[i] = returnState[i] / visitedStatesNum[i]
     return V
+
+# Iteratively updates a value function for an environment using a given policy.
+def iterative_policy_evaluation(env, valueFunction, policy, discountRate, maxNumberOfIterations, convergenceTolerance):
+    for i in range(maxNumberOfIterations):
+        valueFunctionNextIteration = np.zeros(env.observation_space.n)
+        for state in env.P:
+            outerSum = 0
+            for action in env.P[state]:
+                innerSum = 0
+                for probability, nextState, reward, _ in env.P[state][action]:
+                    innerSum += probability * (reward + discountRate * valueFunction[nextState])
+                outerSum += policy[state, action] * innerSum
+            valueFunctionNextIteration[state] = outerSum
+        if np.max(np.abs(valueFunctionNextIteration - valueFunction)) < convergenceTolerance:
+            valueFunction = valueFunctionNextIteration
+            print('Iterative policy evaluation algorithm converged!')
+            break
+        valueFunction = valueFunctionNextIteration
+    return valueFunction
+
+# Visualizes a value function as a heatmap.
+def visualize_valueFuntion(valueFunction,shape,fileNameToSave):
+    ax = sns.heatmap(valueFunction.reshape(shape,shape),
+                     annot=True, square=True,
+                     cbar=False, cmap='Blues',
+                     xticklabels=False, yticklabels=False)
+    plt.savefig(fileNameToSave,dpi=600)
+    plt.show()
